2b6953ce96b27be45a4a2ccb94555918dc24a86f
Erick Birbe <ebirbe@odooconcept.com>
2025-06-13 19:41:37 -0400

    Update __manifest__.py

--- main_app/__manifest__.py
+++ main_app/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Main App",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Extra Tools",
     "summary": "Módulo integrador para el Proyecto",
     "author": "Erick Birbe <ebirbe@odooconcept.com>, Odoo Concept",
