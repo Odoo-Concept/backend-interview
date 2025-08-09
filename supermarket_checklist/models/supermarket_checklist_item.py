bfa4ccdbb1e9c5a17e80e9513c6dc304e7e567b5
juanjricci <jr.juanricci@gmail.com>
2025-08-09 12:08:30 -0300

    solo se pueden agregar y editar items en estado 'borrador'

--- supermarket_checklist/models/supermarket_checklist_item.py
+++ supermarket_checklist/models/supermarket_checklist_item.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 
 
 class SupermarketChecklistItem(models.Model):
@@ -11,12 +11,14 @@ class SupermarketChecklistItem(models.Model):
     product_description = fields.Text(related='product_id.description')
     quantity = fields.Integer(string='Cantidad', required=True)
     bought = fields.Boolean(string='Comprado', default=False)
-    checklist_id = fields.Many2one('supermarket.checklist', string='Lista de Supermercado', required=True)
+    checklist_id = fields.Many2one('supermarket.checklist', string='Lista de Supermercado', required=True, ondelete="cascade")
 
 
     @api.model
     def write(self, vals):
         for record in self:
+            if 'product_id' in vals and record.checklist_id.state != 'draft':
+                raise ValidationError("No puedes editar los items de la lista cuando la lista no está en estado 'Borrador'")
             if self.env.user != record.checklist_id.user_id:
                 raise UserError("No puedes alterar la lista de otro usuario")
         return super().write(vals)
