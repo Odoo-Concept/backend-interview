190111115ba57230f217c23cdd270ca7f8d0bb7d
Felix Daniel Coca Calvimontes <daniel.uremix@gmail.com>
2025-12-12 03:45:53 -0400

    Refactor grocery_store.py: Remove duplicate logging import

--- website_supermarket_list/models/grocery_store.py
+++ website_supermarket_list/models/grocery_store.py
@@ -1,10 +1,10 @@
+import logging
 import re
 
 from odoo import api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.tools import remove_accents
 from odoo.tools.translate import _
-import logging
 
 _logger = logging.getLogger(__name__)
 
